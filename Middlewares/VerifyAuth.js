cc4a3e894f6198d8d3a17247d03372a9cabd739b
kanha-beep <kanhashree2223@gmail.com>
2026-02-09 13:58:51 +0530

    voideo

--- Middlewares/VerifyAuth.js
+++ Middlewares/VerifyAuth.js
@@ -4,7 +4,7 @@ import { ExpressError } from './ExpressError.js';
 export const VerifyAuth = (req, res, next) => {
     try {
         const token = req.cookies.token;
-        console.log("token received: ", token)
+        console.log("token received: ", req.cookies)
         if (!token) return next(new ExpressError(401, "No token generated after login in verifyAuth"));
         // console.log("Token: ", token);
         const decoded = jwt.verify(token, process.env.JWT_SECRET || "study_key");
