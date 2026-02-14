5bb96d292539520e760a556019e6595d67681e94
kanha-beep <kanhashree2223@gmail.com>
2026-02-14 22:32:55 +0530

    chnage

--- Middlewares/IsRole.js
+++ Middlewares/IsRole.js
@@ -1,8 +1,11 @@
-export const IsRole = (...roles) => {
+export const isRole = (...roles) => {
     return (req, res, next) => {
-          console.log("req.user.roles: ", req.user?.roles)
+        console.log("req.user.roles: ", req.user?.roles)
+        if (!req.user || !req.user.roles) {
+            return res.status(401).json({ message: "You are not authenticated" })
+        }
         if (!roles.includes(req.user.roles)) {
-          
+
             return res.status(403).json({ message: "You are not authorized to access this route" })
         }
         next()
--- Routes/Discussion.Routes.js
+++ Routes/Discussion.Routes.js
@@ -1,34 +1,35 @@
 import express from "express"
 import { WrapAsync } from "../Middlewares/WrapAsync.js"
-import { createDiscussion, allDiscussion, singleDiscussion, openChat, allUsers, editDiscussion, allChats, deleteDiscussion, chatMessage, getMessages, ChatBot, createRoom,allRooms, singleRoom, joinRoom, leaveRoom, getRoomMessages, sendRoomMessage } from "../Controllers/Discussion.Controller.js"
+import { createDiscussion, allDiscussion, singleDiscussion, openChat, allUsers, editDiscussion, allChats, deleteDiscussion, chatMessage, getMessages, ChatBot, createRoom, allRooms, singleRoom, joinRoom, leaveRoom, getRoomMessages, sendRoomMessage } from "../Controllers/Discussion.Controller.js"
 import { VerifyAuth } from "../Middlewares/VerifyAuth.js";
+import { isRole } from "../Middlewares/IsRole.js"
 // /api/discussion
 const router = express.Router()
 router.post("/new", WrapAsync(createDiscussion))
 //all users of the platform
-router.get("/all-users", VerifyAuth, WrapAsync(allUsers))
+router.get("/all-users", VerifyAuth, isRole, WrapAsync(allUsers))
 router.get("/", WrapAsync(allDiscussion))
-router.get("/:id", VerifyAuth, WrapAsync(singleDiscussion))
-router.patch("/:id/edit", VerifyAuth, WrapAsync(editDiscussion))
-router.delete("/:id", VerifyAuth, WrapAsync(deleteDiscussion))
+router.get("/:id", VerifyAuth, isRole, WrapAsync(singleDiscussion))
+router.patch("/:id/edit", VerifyAuth, isRole, WrapAsync(editDiscussion))
+router.delete("/:id", VerifyAuth, isRole, WrapAsync(deleteDiscussion))
 //chats
 //all chats of all users
-router.get("/chats", VerifyAuth, allChats)
+router.get("/chats", VerifyAuth, isRole, allChats)
 //open a single chat
-router.post("/chat/:userId", VerifyAuth, openChat);
+router.post("/chat/:userId", VerifyAuth, isRole, openChat);
 //get messages
-router.get("/chat/:chatId/messages", VerifyAuth, getMessages);
+router.get("/chat/:chatId/messages", VerifyAuth, isRole, getMessages);
 //done send message
-router.post("/chat/:chatId/message", VerifyAuth, chatMessage);
-router.post("/chatbot", ChatBot)
+router.post("/chat/:chatId/message", VerifyAuth, isRole, chatMessage);
+router.post("/chatbot", isRole, ChatBot)
 // rooms
-router.post("/room/new", VerifyAuth, WrapAsync(createRoom))
+router.post("/room/new", VerifyAuth, isRole, WrapAsync(createRoom))
 // router.post("/:roomId/room", VerifyAuth, WrapAsync(createRoom))
-router.get("/rooms", VerifyAuth, WrapAsync(allRooms))
-router.get("/room/:roomId", VerifyAuth, WrapAsync(singleRoom))
-router.post("/room/:roomId/join", VerifyAuth, WrapAsync(joinRoom))
-router.post("/room/:roomId/leave", VerifyAuth, WrapAsync(leaveRoom))
+router.get("/rooms", VerifyAuth, isRole, WrapAsync(allRooms))
+router.get("/room/:roomId", VerifyAuth, isRole, WrapAsync(singleRoom))
+router.post("/room/:roomId/join", VerifyAuth, isRole, WrapAsync(joinRoom))
+router.post("/room/:roomId/leave", VerifyAuth, isRole, WrapAsync(leaveRoom))
 // room messages
-router.get("/room/:roomId/messages", VerifyAuth, WrapAsync(getRoomMessages))
-router.post("/room/:roomId/message", VerifyAuth, WrapAsync(sendRoomMessage))
+router.get("/room/:roomId/messages", VerifyAuth, isRole, WrapAsync(getRoomMessages))
+router.post("/room/:roomId/message", VerifyAuth, isRole, WrapAsync(sendRoomMessage))
 export default router
